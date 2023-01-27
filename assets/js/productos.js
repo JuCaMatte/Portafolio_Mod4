df27615f98f8db297ea7141e9be634fa39d17440
JuCaMatte <jusamatte@gmail.com>
2023-01-27 18:13:31 -0300

    first commit

---
+++ assets/js/productos.js
@@ -0,0 +1,132 @@
+let productos = [
+    {
+        id: "0001",
+        nombre: "La sabiduría de los lobos",
+        categoría: "Superación personal",
+        precio: 5900,
+        stock: 10
+    },
+    {
+        id: "0002",
+        nombre: "La confianza en sí mismo",
+        categoría: "Superación personal",
+        precio: 10000,
+        stock: 1155
+    },
+    {
+        id: "0003",
+        nombre: "El libro que ojalá tus padres te hubieran leído",
+        categoría: "Superación personal",
+        precio: 12000,
+        stock: 3
+    },
+
+    {
+      id: "0004",
+      nombre: "El Poder de Confiar en tí",
+      categoría: "Superación personal",
+      precio: 11800,
+      stock: 10,
+
+    },
+    {
+      id: "0005",
+      nombre: "El Pequeño Libro de la Superación Personal 1",
+      categoría: "Superación personal",
+      precio: 43000,
+      stock: 15,
+ 
+    },
+    {
+      id: "0006",
+      nombre: "Nunca te Rindas: El Poder de la Perseverancia",
+      categoría: "Superación personal",
+      precio: 27000,
+      stock: 20,
+
+    },
+    {
+      id: "0007",
+      nombre: "El hombre más feliz del mundo",
+      categoría: "Superación personal",
+      precio: 20000,
+      stock: 17,
+
+    },
+    {
+      id: "0008",
+      nombre: "Libro Mala Madre: Malcriando Sin Culpas",
+      categoría: "Superación personal",
+      precio: 40000,
+      stock: 30,
+
+    },
+    {
+      id: "0009",
+      nombre: "La paciente silenciosa",
+      categoría: "Misterio",
+      precio: 17050,
+      stock: 18,
+
+    },
+    {
+      id: "0010",
+      nombre: "Reina roja",
+      categoría: "Misterio",
+      precio: 17050,
+      stock: 25,
+
+    },
+    {
+      id: "0011",
+      nombre: "Los hombres que no amaban a las mujeres",
+      categoría: "Misterio",
+      precio: 25000,
+      stock: 18,
+   
+    },
+    {
+      id: "0012",
+      nombre: "El código Da Vinci",
+      categoría: "Misterio",
+      precio: 21290,
+      stock: 20,
+   
+    },
+    {
+      id: "0013",
+      nombre: "La sombra del viento",
+      categoría: "Misterio",
+      precio: 16400,
+      stock: 14,
+
+    },
+    {
+      id: "0014",
+      nombre: "La noche en que todo cambió",
+      categoría: "Misterio",
+      precio: 22970,
+      stock: 23,
+
+    },
+    {
+      id: "0015",
+      nombre: "El espía que surgió del frío",
+      categoría: "Misterio",
+      precio: 13840,
+      stock: 15,
+
+    },
+    {
+      id: "0016",
+      nombre: "El visitante",
+      categoría: "Misterio",
+      precio: 18860,
+      stock: 25,
+
+    },
+
+
+]
+
+export default productos;
